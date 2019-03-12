cc985eb14ef8991ea3030af83495a4dd15f3b5a5
Taylor Bradley <trbradley48@gmail.com>
2019-03-11 17:58:14 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,26 @@
 
 
 // Declaration of variables
-var wordArray = [];
-var wordSelector = Math.random();
+var wordArray = ["metallica", "sabath"];
+var wordSelected = wordArray[Math.floor(Math.random() * wordArray.length)];
+var underscoreWord = [];
+
+// Create array filled with '_' for selected word
+for (var i = 0; i < wordSelected.length; i++) {
+    underscoreWord[i] = "_";
+}
+
+console.log(wordSelected);
+console.log(underscoreWord);
+
 
 // Declaration of text varialbes in HTML
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
 var userGuessText = document.getElementById("userGuess-text");
+var underscoreWordText = document.getElementById("underscoreWord-text");
+
+underscoreWordText.textContent = underscoreWord.toString();
 
 // This function is run whenever the user presses a key
 document.onkeyup = function(event) {
@@ -15,12 +28,22 @@ document.onkeyup = function(event) {
 
     // Determines which key was pressed
     var userGuess = event.key;
-    console.log(userGuess);
+    // console.log(userGuess);
+    // console.log(event);
 
 
-    if (userGuess === 'a') {
+    if (65 <= userGuess <= 90 ) {
         // Display the letter guessed by user
+        directionsText.textContent = "";
         userGuessText.textContent = "You chose: " + userGuess;
+
+        for (var j = 0; j < wordSelected.length; j++) {
+            if (wordSelected.charAt(j) === userGuess) {
+                underscoreWord[j] = userGuess;
+            }
+        }
+        console.log(underscoreWord);
+        underscoreWordText.textContent = underscoreWord.toString();
     }
     
 
